53cab0e1b8bd3fa5174fba4561f4e35e422bff72
nickgolovkov <nick.golovkov@mail.ru>
2018-03-31 21:29:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# job_first_project
---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+$('#header-order-call-viber-image').hover(function(){
+  $(this).attr('src','images/%D1%85%D0%BE%D0%B2%D0%B5%D1%80-%D0%B2%D0%B0%D0%B9%D0%B1%D0%B5%D1%80.png');
+},function(){
+ $(this).attr('src','images/%D0%B2%D0%B0%D0%B9%D0%B1%D0%B5%D1%80.png' );
+})
